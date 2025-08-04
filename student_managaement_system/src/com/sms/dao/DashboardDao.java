e3ddfd0d30bbd68b6455169181b16da2ede0b7bd
Ashish Dabhi <ashishkumar.dabhi@tssconsultancy.com>
2025-08-04 15:51:53 +0530

    Done with Dashboard

--- student_managaement_system/src/com/sms/dao/DashboardDao.java
+++ student_managaement_system/src/com/sms/dao/DashboardDao.java
@@ -10,7 +10,6 @@ import java.util.List;
 import com.sms.database.DBConnection;
 import com.sms.model.DashboardModel;
 
-
 public class DashboardDao {
 
 	private Connection connection;
@@ -27,29 +26,28 @@ public class DashboardDao {
 		List<DashboardModel> dashboardList = new ArrayList<>();
 
 		String query = """
-																      SELECT
+									SELECT
 				    s.student_id,
-				    s.name AS name,
-				    ANY_VALUE(c.course_name) AS course,
-				    ANY_VALUE(f.paid_amount) AS paidFee,               
-				    ANY_VALUE(f.pending_amount) AS pendingFee,         
-				    ANY_VALUE(f.total_fee) AS totalFee,                
+				    s.name AS student_name,
+				    GROUP_CONCAT(DISTINCT c.course_name) AS courses,
+				    SUM(DISTINCT f.total_fee) AS total_fee,
+				    SUM(DISTINCT f.paid_amount) AS total_paid_fee,
+				    SUM(DISTINCT f.pending_amount) AS total_pending_fee,
 				    GROUP_CONCAT(DISTINCT sub.subject_name) AS subjects,
 				    GROUP_CONCAT(DISTINCT t.name) AS teachers
 				FROM students s
 				JOIN student_courses sc ON s.student_id = sc.student_id
 				JOIN courses c ON sc.course_id = c.course_id
-				LEFT JOIN fees f ON sc.student_course_id = f.student_course_id
+				LEFT JOIN fees f ON f.student_course_id = sc.student_course_id
 				LEFT JOIN student_subjects ss ON sc.student_course_id = ss.student_course_id
 				LEFT JOIN subject_course subj_c ON ss.subject_course_id = subj_c.id
 				LEFT JOIN subjects sub ON subj_c.subject_id = sub.subject_id
 				LEFT JOIN subject_teachers st ON sub.subject_id = st.subject_id
 				LEFT JOIN teachers t ON st.teacher_id = t.teacher_id
 				WHERE s.is_active = TRUE
-				GROUP BY s.student_id;
+				GROUP BY s.student_id, s.name;
 
-
-																        """;
+								""";
 
 		try (PreparedStatement ps = connection.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
 
@@ -59,11 +57,11 @@ public class DashboardDao {
 				DashboardModel model = new DashboardModel();
 				model.setSrNo(++srNo);
 				model.setStudentId(rs.getInt("student_id"));
-				model.setName(rs.getString("name"));
-				model.setCourse(rs.getString("course"));
-				model.setPaidFee(rs.getDouble("paidFee"));
-				model.setPendingFee(rs.getDouble("pendingFee"));
-				model.setTotalFee(rs.getDouble("totalFee"));
+				model.setName(rs.getString("student_name"));
+				model.setCourse(rs.getString("courses"));
+				model.setPaidFee(rs.getDouble("total_paid_fee"));
+				model.setPendingFee(rs.getDouble("total_pending_fee"));
+				model.setTotalFee(rs.getDouble("total_fee"));
 				model.setSubjects(rs.getString("subjects"));
 				model.setTeachers(rs.getString("teachers"));
 
